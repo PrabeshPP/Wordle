72cf33b0aa9effc96e3b90594b98f37e5726df4c
PrabeshPP <prabesh.bista369@gmail.com>
2023-02-25 01:35:21 -0500

    [added] guessing algorithm

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val wordToGuess="Prab"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //function to guess the word
+    private fun checkGuess(guess: String) : String {
+
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
     }
 }
